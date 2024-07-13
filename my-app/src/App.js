c3736b9638635d94b37273d20f8f150103b0d943
ZhannaKhizrieva <zhanna-apteka@mail.ru>
2024-07-13 20:20:40 +0300

    add unmounting

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,40 @@
 import { useEffect, useState } from 'react';
-import styles from './app.css';
+import styles from './app.module.css';
 
 export const App = () => {
-	const [products, setProducts] = useState([]);
+	const [counter, setCounter] = useState(0);
 
 	useEffect(() => {
-		fetch('httt:/')
-			.then((loadeData) => loadeData.json())
-			.then((loadeProducts) => {
-				setProducts(loadeProducts);
-			});
-	}, []);
+		console.log('Первый-' + counter);
+
+		return () => console.log('Второй-' + counter);
+	}, [counter]);
 
 	return (
 		<div className={styles.app}>
-			{products.map(({ id, name, price }) => (
-				<div кеу={id}>
-					{name} - {price} руб
-				</div>
-			))}
+			<button onClick={() => setCounter(counter + 1)}>+ 1</button>
 		</div>
 	);
 };
+
+/*
+
+[
+	{
+	"id": "001",
+	"name": "Телевизор",
+	"price": 39900
+	},
+	{
+	"id": "002",
+	"name": "Смартфон",
+	"price": 18900
+	},
+	{
+	"id": "003",
+	"name": "Фен",
+	"price": 1749
+	}
+]
+
+*/
