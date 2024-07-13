082536901fa6b141a86e2ea71b47d626ac1bb3c4
ZhannaKhizrieva <zhanna-apteka@mail.ru>
2024-07-13 18:51:04 +0300

    add useEffect

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,24 @@
-import './app.css';
+import { useEffect, useState } from 'react';
+import styles from './app.css';
 
 export const App = () => {
+	const [products, setProducts] = useState([]);
+
+	useEffect(() => {
+		fetch('httt:/')
+			.then((loadeData) => loadeData.json())
+			.then((loadeProducts) => {
+				setProducts(loadeProducts);
+			});
+	}, []);
+
 	return (
-		<div className="App">
-			<h>Привет! Я пишу в реакте.</h>
+		<div className={styles.app}>
+			{products.map(({ id, name, price }) => (
+				<div кеу={id}>
+					{name} - {price} руб
+				</div>
+			))}
 		</div>
 	);
 };
