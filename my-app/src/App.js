5ffa43697fd25a752b34ac9ea9a67793b3b1c2c0
ZhannaKhizrieva <zhanna-apteka@mail.ru>
2024-07-14 11:32:25 +0300

    add PRODUCTS_MOCK

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,54 @@
 import { useEffect, useState } from 'react';
 import styles from './app.module.css';
 
+const PRODUCTS_MOCK = [
+	{
+		id: '001',
+		name: 'Телевизор',
+		price: 39900,
+	},
+	{
+		id: '002',
+		name: 'Смартфон',
+		price: 18900,
+	},
+	{
+		id: '003',
+		name: 'Фен',
+		price: 1749,
+	},
+];
+
 export const App = () => {
-	const [counter, setCounter] = useState(0);
+	const [products, setProducts] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
 
 	useEffect(() => {
-		console.log('Первый-' + counter);
+		setIsLoading(true);
 
-		return () => console.log('Второй-' + counter);
-	}, [counter]);
+		new Promise((resolve) => {
+			setTimeout(() => {
+				resolve({ json: () => PRODUCTS_MOCK });
+			}, 5000);
+		})
+			.then((loadedData) => loadedData.json())
+			.then((loadedProducts) => {
+				setProducts(loadedProducts);
+			})
+			.finally(() => setIsLoading(false));
+	}, []);
 
 	return (
 		<div className={styles.app}>
-			<button onClick={() => setCounter(counter + 1)}>+ 1</button>
+			{isLoading ? (
+				<div className={styles.loader}></div>
+			) : (
+				products.map(({ id, name, price }) => (
+					<div key={id}>
+						{name} - {price} руб,
+					</div>
+				))
+			)}
 		</div>
 	);
 };
